c0588436f98289ff189d3b993f291c07394bf084
kdh-work <110156080+kdh-work@users.noreply.github.com>
2022-08-23 14:52:23 +0900

    일부 수정 및 vue 예제 제작
    do it vue 예제 일부 수정
    vue 관련 예제 새로 제작

---
+++ exprops/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>상위 컴포넌트</h1>
+    상위 컴포넌트가 보내는 값: {{send_name}} <!--상위에서 보내는 데이터값-->
+    <child_compo :get_name="send_name"></child_compo> <!--상위에서 보낸 데이터를 받은 하위 컴포넌트-->
+  </div>
+</template>
+
+<script>
+//import 지정하고 싶은 이름 from "경로"
+import child_compo from "./components/child_compo.vue"//child_compo 컴포넌트 사용을 위해
+export default { //import를 사용하기 때문에 export 사용
+  name: 'parent_compo', 
+  components: {
+    //'지정하고싶은 이름' : import에서 지정한 이름
+    'child_compo' : child_compo //import한 child_compo를 하위 컴포넌트로 사용
+
+  },
+  data() {
+    return{
+      send_name: '전달값' //해당 뷰에서 사용할 데이터를 지님
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ exprops/src/components/child_compo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    하위 컴포넌트가 받은 값: {{get_name}} <!--props를 통해 전달 받은 값 화면 등록-->
+  </div>
+</template>
+
+<script>
+  export default {
+   //props 속성을 사용하기 위해 하위 컴포넌트에 속성 정의
+  props: ['get_name'], //상위 컴포넌트에서 보낸 데이터를 받아서 template에 적용
+  }
+</script>
+
+<style>
+
+</style>
