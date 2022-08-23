8a3079acad4fef7bbdc9a8b6cf063c6166339082
kdh-work <110156080+kdh-work@users.noreply.github.com>
2022-08-23 18:04:32 +0900

    vue 예제
    vue props 예제 제작
    vue event 예제 제작중

---
+++ exevent/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h2>상위 컴포넌트</h2>
+
+    <!--v-on:하위 컴포넌트의 이벤트명="상위 컴포넌트의 메서드명"-->
+    <child_com v-on:checkchild="checkParent"></child_com>
+    <!--<child_com v-bind:count="printText" v-on:change="change"></child_com>-->
+  </div>
+</template>
+
+<script>
+import child_com from "./components/child_com.vue"
+export default {
+  components: {
+    child_com
+  },
+  methods:{
+    checkParent(text) {
+      alert(text);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ exevent/src/components/child_com.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <checkParent><input type="button" v-on:onclick="checkParent()"></checkParent>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      checkParent() {
+        let text="상위 컴포넌트로 전달하는 데이터"
+        this.$emit(checkchild, text)
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
